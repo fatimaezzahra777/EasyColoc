6859d427895b7a61e93dca252e6095dc8002260c
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-02 11:33:39 +0000

    ajoute des modifications dans les controller

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Models\Invitation;
 use App\Models\Colocation;
 use App\Models\Membership;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+use App\Mail\ColocationInvitationMail;
+
 
 class InvitationController extends Controller
 {
@@ -22,20 +26,37 @@ class InvitationController extends Controller
 
         return view('invitations.index', compact('colocation', 'invitations'));
     }
+
+    public function store(Request $request, Colocation $colocation)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+        ]);
+
+        $invitation = Invitation::create([
+            'colocation_id' => $colocation->id,
+            'email' => $request->email,
+            'token' => (string) Str::uuid(),
+        ]);
+
+        Mail::to($request->email)->send(new ColocationInvitationMail($invitation));
+
+        return redirect()
+            ->route('invitations.index', $colocation)
+            ->with('success', 'Invitation envoyée');
+    }
     public function accept($token)
     {
         $invitation = Invitation::where('token', $token)
-                                ->where('expires_at', '>', now())
+                
                                 ->firstOrFail();
 
-        // Ajouter le membre à la colocation
         Membership::create([
             'user_id' => auth()->id(),
             'colocation_id' => $invitation->colocation_id,
             'role' => 'member',
         ]);
 
-        // Supprimer l’invitation
         $invitation->delete();
 
         return redirect()->route('dashboard')->with('success', 'Vous avez rejoint la colocation !');
