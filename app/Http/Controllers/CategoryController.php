0668d17afb2c7a918d2d4074bd5708f95f0afc86
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-02 11:32:27 +0000

    ajoute des modifications dans les controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Colocation;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(Colocation $colocation)
+    {
+        $categories = $colocation->categories; // récupère toutes les catégories de la coloc
+        return view('categories.index', compact('colocation', 'categories'));
+    }
+
+    public function create(Colocation $colocation)
+    {
+        return view('categories.create', compact('colocation'));
+    }
+    public function store(Request $request, Colocation $colocation)
+    {
+        
+
+        $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'max:100',
+                'unique:categories,name,NULL,id,colocation_id,' . $colocation->id,
+            ],
+        ]);
+
+        $colocation->categories()->create([
+            'name' => $request->name,
+        ]);
+
+        return back()->with('success', 'Catégorie ajoutée avec succès.');
+    }
+
+    public function destroy(Category $category)
+    {
+        
+
+        if ($category->depenses()->exists()) {
+            return back()->withErrors('Cette catégorie est utilisée.');
+        }
+
+        $category->delete();
+
+        return back();
+    }
+
+
+
 }
