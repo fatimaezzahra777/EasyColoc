d1af1fe76a7caa67929080810549b681dcc850d1
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-04 23:35:04 +0000

    ajoute des modification dans les controllers

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -2,27 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Colocation;
+use App\Models\Depenses;
+use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
-    public function index()
+    /* afficher les users avec les vraies infos */
+
+    public function index(Request $request)
     {
-        $users = User::all();
-        return view('admin.users', compact('users'));
+        $query = User::query();
+        $query->where('is_global_admin', false);
+        $query->withCount('colocations');
+            
+        
+        if ($search = $request->input('search')) {
+            $query->where(function($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                ->orWhere('email', 'like', "%{$search}%");
+            });
+        }
+
+        $users = $query->get();
+        $stats = [
+            'users'       => User::count(),
+            'colocations' => Colocation::count(),
+            'expenses'    => Depenses::count(),
+            'volume'      => Depenses::sum('montant'),
+        ];
+
+        $recentUsers = User::latest()->take(5)->get();
+
+        return view('admin.users', compact('users', 'stats',  'recentUsers'));
     }
 
+    /* bannir */
     public function ban(User $user)
     {
-        $user->update(['is_banned' => true]);
-        return redirect()->back()->with('success', 'Utilisateur banni avec succès.');
+        $user->update([
+            'is_banned' => true
+        ]);
+
+        return back()->with('success', 'Utilisateur banni avec succès.');
     }
 
+    /* débannir */
     public function unban(User $user)
     {
-        $user->update(['is_banned' => false]);
-        return redirect()->back()->with('success', 'Utilisateur débanni avec succès.');
+        $user->update([
+            'is_banned' => false
+        ]);
+
+        return back()->with('success', 'Utilisateur débanni avec succès.');
     }
 }
-
