618b30667a72a4889befbbd862487eb9aebcb72a
fatimaezzahra777 <fatimaezzahrabel777@gmail.com>
2026-03-04 23:39:41 +0000

    ajoute des modification dans les controllers

--- app/Http/Controllers/DepensesController.php
+++ app/Http/Controllers/DepensesController.php
@@ -12,12 +12,9 @@ class DepensesController extends Controller
 {
     public function index(Colocation $colocation)
     {
-        // très important : on affiche uniquement
-        // les dépenses de cette colocation
-        $depenses = Depenses::where('colocation_id', $colocation->id)
-            ->with('category')
-            ->latest()
-            ->paginate(10);
+        $depenses = Depenses::with('category', 'user', 'colocation')
+                ->where('colocation_id', $colocation->id)
+                ->paginate(10);
 
         return view('depenses.index', compact('colocation', 'depenses'));
     }
@@ -53,5 +50,54 @@ class DepensesController extends Controller
 
         return redirect()->route('depenses.index', $colocation)
                         ->with('success', 'Dépense enregistrée !');
+    }
+
+    public function balances(Colocation $colocation)
+    {
+        $members = $colocation->users;
+
+        $balances = [];
+
+        foreach ($members as $user) {
+            $balances[$user->id] = 0;
+        }
+
+        foreach ($colocation->depenses as $depense) {
+
+            $payerId = $depense->user_id;
+
+            if (!isset($balances[$payerId])) {
+                continue;
+            }
+
+            $part = $depense->montant / $members->count();
+
+            foreach ($members as $member) {
+
+                if (!isset($balances[$member->id])) {
+                    $balances[$member->id] = 0;
+                }
+
+                if ($member->id == $payerId) {
+                    $balances[$member->id] += $depense->montant - $part;
+                } else {
+                    $balances[$member->id] -= $part;
+                }
+            }
+        }
+
+        $result = [];
+
+        foreach ($members as $user) {
+            $result[] = [
+                'user'    => $user,
+                'balance' => round($balances[$user->id], 2),
+            ];
+        }
+
+        return view('balances.index', [
+            'colocation' => $colocation,
+            'balances'   => $result,
+        ]);
     }
 }
